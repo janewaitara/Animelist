7824ae49b9136443f1ac6caea6b3c2d0f1452157
Waitara <janewaitara99@gmail.com>
2023-06-24 01:21:58 +0300

    Updated data  module's build.gradle file to use libs file dependencies

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -9,7 +9,6 @@ android {
 
     defaultConfig {
         minSdk = 24
-        targetSdk = 33
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -35,12 +34,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(libs.androidx.core.ktx)
 
     // Enables formatting rule set in detekt
     detektPlugins(libs.detekt.formatting)

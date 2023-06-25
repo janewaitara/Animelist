c53b95fd31eb92b8c02ea8226afee2949764a27d
Waitara <janewaitara99@gmail.com>
2023-06-25 18:01:04 +0300

    Configured apollograpgql in the network module

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint")
     id("io.gitlab.arturbosch.detekt")
     id("dagger.hilt.android.plugin")
+    id("com.apollographql.apollo3") version "3.8.2"
     id("kotlin-kapt")
 }
 
@@ -48,7 +49,16 @@ dependencies {
     kapt(libs.hilt.android.compiler)
     kapt(libs.hilt.compiler)
 
+    // ApolloGraphql
+    implementation(libs.apollographql)
+
     testImplementation(libs.junit4)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso)
+}
+
+apollo {
+    service("service") {
+        packageName.set("com.mumbicodes.network")
+    }
 }

5b13452bd2e49bf8f0eb2699dcb0335b4d3ebc72
Waitara <janewaitara99@gmail.com>
2023-06-27 00:58:30 +0300

    Updated anime repo parameters

--- core/domain/src/main/java/com/mumbicodes/domain/repository/AnimeRepository.kt
+++ core/domain/src/main/java/com/mumbicodes/domain/repository/AnimeRepository.kt
@@ -3,13 +3,22 @@ package com.mumbicodes.domain.repository
 import com.mumbicodes.network.AnimeListQuery
 import com.mumbicodes.network.AnimeQuery
 import com.mumbicodes.network.RecommendationsQuery
+import com.mumbicodes.network.type.MediaFormat
+import com.mumbicodes.network.type.MediaSort
+import com.mumbicodes.network.type.MediaType
 import kotlinx.coroutines.flow.Flow
 
 interface AnimeRepository {
 
-    fun getAnimeList(): Flow<List<AnimeListQuery.Medium>>
+    fun getAnimeList(
+        page: Int?,
+        perPage: Int?,
+        type: MediaType?,
+        sortList: List<MediaSort>?,
+        formatIn: List<MediaFormat>?,
+    ): Flow<List<AnimeListQuery.Medium>>
 
     fun getRecommendations(): Flow<List<RecommendationsQuery.Media>>
 
-    fun getAnime(animeId: Int, page: Int, perPage: Int): Flow<AnimeQuery.Media>
+    fun getAnime(animeId: Int, page: Int?, perPage: Int?): Flow<AnimeQuery.Media>
 }

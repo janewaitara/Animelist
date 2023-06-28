28696e7b89dbd92fc8301f3cfdf077e54030eaac
Waitara <janewaitara99@gmail.com>
2023-06-28 17:53:24 +0300

    Added implementation to fetch character details

---
+++ core/data/src/main/java/com/mumbicodes/data/repository/CharacterRepositoryImpl.kt
@@ -0,0 +1,21 @@
+package com.mumbicodes.data.repository
+
+import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.api.Optional
+import com.apollographql.apollo3.cache.normalized.FetchPolicy
+import com.apollographql.apollo3.cache.normalized.fetchPolicy
+import com.mumbicodes.common.result.Result
+import com.mumbicodes.common.result.asResult
+import com.mumbicodes.domain.repository.CharacterRepository
+import com.mumbicodes.network.CharacterQuery
+import kotlinx.coroutines.flow.Flow
+
+class CharacterRepositoryImpl(private val apolloClient: ApolloClient) : CharacterRepository {
+    override fun getCharacter(characterId: Int): Flow<Result<CharacterQuery.Character>> {
+        return apolloClient.query(
+            CharacterQuery(characterId = Optional.present(characterId))
+        ).fetchPolicy(FetchPolicy.NetworkFirst).toFlow().asResult {
+            it.Character!!
+        }
+    }
+}

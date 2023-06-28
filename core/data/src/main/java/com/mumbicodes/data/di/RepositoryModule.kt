7bd43f0744e00353afc6a2c97dbd3caf612ff6d0
Waitara <janewaitara99@gmail.com>
2023-06-28 18:36:24 +0300

    Added a repository DI module

---
+++ core/data/src/main/java/com/mumbicodes/data/di/RepositoryModule.kt
@@ -0,0 +1,34 @@
+package com.mumbicodes.data.di
+
+import com.apollographql.apollo3.ApolloClient
+import com.mumbicodes.data.repository.AnimeRepositoryImpl
+import com.mumbicodes.data.repository.CharacterRepositoryImpl
+import com.mumbicodes.data.repository.SearchRepositoryImpl
+import com.mumbicodes.domain.repository.AnimeRepository
+import com.mumbicodes.domain.repository.CharacterRepository
+import com.mumbicodes.domain.repository.SearchRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideSearchRepository(apolloClient: ApolloClient): SearchRepository =
+        SearchRepositoryImpl(apolloClient = apolloClient)
+
+    @Provides
+    @Singleton
+    fun provideCharacterRepository(apolloClient: ApolloClient): CharacterRepository =
+        CharacterRepositoryImpl(apolloClient = apolloClient)
+
+    @Provides
+    @Singleton
+    fun provideAnimeRepository(apolloClient: ApolloClient): AnimeRepository =
+        AnimeRepositoryImpl(apolloClient = apolloClient)
+}

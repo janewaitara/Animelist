8da97e35fcbe39ef8eab864463bfa4a60ddf48ff
Waitara <janewaitara99@gmail.com>
2023-06-28 16:56:15 +0300

    Added implementation to fetch an anime

--- core/common/src/main/java/com/mumbicodes/common/result/Result.kt
+++ core/common/src/main/java/com/mumbicodes/common/result/Result.kt
@@ -1,11 +1,10 @@
 package com.mumbicodes.common.result
 
 import com.apollographql.apollo3.api.Error
-import com.apollographql.apollo3.exception.ApolloException
 
 sealed interface Result<out T> {
     object Loading : Result<Nothing>
     data class Success<T>(val data: T) : Result<T>
-    data class ProtocolError(val exception: ApolloException) : Result<Nothing>
+    data class Failure(val exception: Throwable) : Result<Nothing>
     data class ApplicationError(val errors: List<Error>) : Result<Nothing>
 }
--- core/data/src/main/java/com/mumbicodes/data/repository/AnimeRepositoryImpl.kt
+++ core/data/src/main/java/com/mumbicodes/data/repository/AnimeRepositoryImpl.kt
@@ -1,6 +1,9 @@
 package com.mumbicodes.data.repository
 
 import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.api.Optional
+import com.apollographql.apollo3.cache.normalized.FetchPolicy
+import com.apollographql.apollo3.cache.normalized.fetchPolicy
 import com.mumbicodes.common.result.Result
 import com.mumbicodes.domain.repository.AnimeRepository
 import com.mumbicodes.network.AnimeListQuery
@@ -9,8 +12,12 @@ import com.mumbicodes.network.RecommendationsQuery
 import com.mumbicodes.network.type.MediaFormat
 import com.mumbicodes.network.type.MediaSort
 import com.mumbicodes.network.type.MediaType
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.mapLatest
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class AnimeRepositoryImpl(private val apolloClient: ApolloClient) : AnimeRepository {
     override fun getAnimeList(
         page: Int?,
@@ -27,6 +34,25 @@ class AnimeRepositoryImpl(private val apolloClient: ApolloClient) : AnimeReposit
     }
 
     override fun getAnime(animeId: Int, page: Int?, perPage: Int?): Flow<Result<AnimeQuery.Media>> {
-        TODO("")
+        val response = apolloClient.query(
+            AnimeQuery(
+                mediaId = Optional.present(animeId),
+                page = Optional.presentIfNotNull(page),
+                perPage = Optional.presentIfNotNull(perPage)
+            )
+        ).fetchPolicy(FetchPolicy.NetworkFirst)
+            .toFlow()
+
+        return response.mapLatest {
+            if (it.hasErrors()) {
+                Result.ApplicationError(it.errors!!)
+            } else if (it.data != null) {
+                Result.Success(it.data!!.Media!!)
+            } else {
+                error("Unknown error occurred. There was no data or errors received")
+            }
+        }.catch {
+            Result.Failure(it)
+        }
     }
 }

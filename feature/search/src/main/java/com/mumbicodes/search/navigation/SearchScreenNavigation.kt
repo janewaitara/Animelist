e5a1909c3adcf007daafb0ed31aa187f6abad8e3
Waitara <janewaitara99@gmail.com>
2023-07-03 08:25:18 +0300

    Added a SearchScreen NavGraphBuilder

---
+++ feature/search/src/main/java/com/mumbicodes/search/navigation/SearchScreenNavigation.kt
@@ -0,0 +1,20 @@
+package com.mumbicodes.search.navigation
+
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.mumbicodes.search.SearchScreen
+
+const val SEARCH_SCREEN_ROUTE = "search_route"
+
+fun NavController.navigateToSearchScreen(navOptions: NavOptions? = null) {
+    this.navigate(route = SEARCH_SCREEN_ROUTE, navOptions = navOptions)
+}
+
+fun NavGraphBuilder.searchScreen() {
+    composable(route = SEARCH_SCREEN_ROUTE) {
+        SearchScreen(modifier = Modifier)
+    }
+}

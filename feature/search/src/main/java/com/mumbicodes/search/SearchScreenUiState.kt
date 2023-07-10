8ff029f274e8b6df468b4ce4ec31a3fbb964b881
Waitara <janewaitara99@gmail.com>
2023-07-10 22:54:56 +0300

    Added an CharacterSearchUiState to hold the state of the character searches

--- feature/search/src/main/java/com/mumbicodes/search/SearchScreenUiState.kt
+++ feature/search/src/main/java/com/mumbicodes/search/SearchScreenUiState.kt
@@ -1,6 +1,7 @@
 package com.mumbicodes.search
 
 import com.mumbicodes.network.SearchAnimeQuery
+import com.mumbicodes.network.SearchCharacterQuery
 
 sealed interface AnimeSearchUiState {
     object Loading : AnimeSearchUiState
@@ -10,4 +11,14 @@ sealed interface AnimeSearchUiState {
     ) : AnimeSearchUiState
 
     data class Error(val errorMessage: String) : AnimeSearchUiState
+}
+
+sealed interface CharacterSearchUiState {
+    object Loading : CharacterSearchUiState
+
+    data class CharacterResults(
+        val data: List<SearchCharacterQuery.Character>
+    ) : CharacterSearchUiState
+
+    data class Error(val errorMessage: String) : CharacterSearchUiState
 }

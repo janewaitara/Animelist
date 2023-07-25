84ab2d30b0aed254c06c3d615d3bdd1fe138e6c9
Waitara <janewaitara99@gmail.com>
2023-07-26 00:29:37 +0300

    Added an extension function to configure base Kotlin with android options

---
+++ build-logic/convention/src/main/kotlin/com/mumbicodes/KotlinAndroid.kt
@@ -0,0 +1,58 @@
+package com.mumbicodes
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.provideDelegate
+import org.gradle.kotlin.dsl.withType
+
+/**
+ * Configure base Kotlin with Android options
+ */
+fun Project.configureKotlinAndroid(commonExtension: CommonExtension<*, *, *, *>,) {
+    commonExtension.apply {
+        compileSdk = 33
+
+        defaultConfig{
+            minSdk = 24
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
+
+        configureKotlin()
+
+        dependencies {
+            add("implementation", libs.findLibrary("androidx.core.ktx").get())
+
+            add("testImplementation", libs.findLibrary("junit4").get())
+            add("androidTestImplementation", libs.findLibrary("androidx.junit").get())
+            add("androidTestImplementation", libs.findLibrary("androidx.espresso").get())
+        }
+    }
+}
+
+/**
+ * Configure base Kotlin options
+ */
+fun Project.configureKotlin(){
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = JavaVersion.VERSION_11.toString()
+
+            // Treat all Kotlin warnings as errors (disabled by default)
+            // Override by setting warningsAsErrors=true in your ~/.gradle/gradle.properties
+            val warningsAsErrors: String? by project
+            allWarningsAsErrors = warningsAsErrors.toBoolean()
+            freeCompilerArgs = freeCompilerArgs + listOf(
+                "-opt-in=kotlin.RequiresOptIn",
+                // Enable experimental coroutines APIs, including Flow
+                "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+                "-opt-in=kotlinx.coroutines.FlowPreview",
+            )
+        }
+    }
+}

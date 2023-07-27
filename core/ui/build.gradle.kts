682b7164dfeda1c60a3e047b769878765ebf9ccd
Waitara <janewaitara99@gmail.com>
2023-07-27 16:46:47 +0300

    Updated the build.gradle in core:ui module to use the androidLibraryPlugin

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -1,17 +1,13 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id("animelist.android.library")
     id("org.jlleitschuh.gradle.ktlint")
     id("io.gitlab.arturbosch.detekt")
 }
 
 android {
     namespace = "com.mumbicodes.ui"
-    compileSdk = 33
 
     defaultConfig {
-        minSdk = 24
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -25,24 +21,11 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appCompat)
 
     // Enables formatting rule set in detekt
     detektPlugins(libs.detekt.formatting)
-
-    testImplementation(libs.junit4)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso)
 }

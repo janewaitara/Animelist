127c2c072ecfdef7fc00f4ea49dd44df23d6efbe
Waitara <janewaitara99@gmail.com>
2023-07-27 21:34:39 +0300

    Updated the build.gradle in feature:home  module to use the androidLibraryPlugin

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -1,8 +1,5 @@
-import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
-
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id("animelist.android.library")
     id("org.jlleitschuh.gradle.ktlint")
     id("io.gitlab.arturbosch.detekt")
     id("dagger.hilt.android.plugin")
@@ -11,11 +8,8 @@ plugins {
 
 android {
     namespace = "com.mumbicodes.home"
-    compileSdk = 33
 
     defaultConfig {
-        minSdk = 24
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -28,13 +22,6 @@ android {
                 "proguard-rules.pro"
             )
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
@@ -50,7 +37,6 @@ dependencies {
     implementation(project(":core:network"))
     implementation(project(":core:common"))
 
-    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appCompat)
 
     val composeBom = platform(libs.compose.bom)
@@ -67,10 +53,6 @@ dependencies {
     kapt(libs.hilt.android.compiler)
     kapt(libs.hilt.compiler)
     implementation(libs.hilt.navigation)
-
-    testImplementation(libs.junit4)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso)
 
     // Enables formatting rule set in detekt
     detektPlugins(libs.detekt.formatting)

1c6ec6ca98f821a528ad9463d7f9ff15ec273ce0
Waitara <janewaitara99@gmail.com>
2023-07-27 23:48:15 +0300

    Updated the build.gradle in feature:character module to use the androidHiltPlugin

--- feature/character/build.gradle.kts
+++ feature/character/build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
     id("animelist.android.library")
+    id("animelist.android.hilt")
     id("org.jlleitschuh.gradle.ktlint")
     id("io.gitlab.arturbosch.detekt")
-    id("dagger.hilt.android.plugin")
-    id("kotlin-kapt")
 }
 
 android {
@@ -47,9 +46,6 @@ dependencies {
     implementation(libs.accompanist)
 
     // Hilt
-    implementation(libs.hilt.android)
-    kapt(libs.hilt.android.compiler)
-    kapt(libs.hilt.compiler)
     implementation(libs.hilt.navigation)
 
     // Enables formatting rule set in detekt

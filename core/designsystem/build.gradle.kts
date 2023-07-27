6cc1699a6b15e4ed04bf5bf4070997a389139b9c
Waitara <janewaitara99@gmail.com>
2023-07-27 16:59:00 +0300

    Updated the build.gradle in core:designsystem module to use the androidLibraryPlugin

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,17 +1,13 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id("animelist.android.library")
     id("org.jlleitschuh.gradle.ktlint")
     id("io.gitlab.arturbosch.detekt")
 }
 
 android {
     namespace = "com.mumbicodes.designsystem"
-    compileSdk = 33
 
     defaultConfig {
-        minSdk = 24
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -25,20 +21,11 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
-
     val composeBom = platform(libs.compose.bom)
 
-    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appCompat)
     implementation(libs.bundles.compose)
     implementation(libs.compose.navigation)
@@ -46,8 +33,4 @@ dependencies {
 
     // Enables formatting rule set in detekt
     detektPlugins(libs.detekt.formatting)
-
-    testImplementation(libs.junit4)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso)
 }

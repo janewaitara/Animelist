4825ee549ca785880b3d021bb814831bbafc8f69
Waitara <janewaitara99@gmail.com>
2023-07-27 20:58:04 +0300

    Updated the build.gradle in core:common module to use the androidLibraryPlugin

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -1,17 +1,13 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id("animelist.android.library")
     id("org.jlleitschuh.gradle.ktlint")
     id("io.gitlab.arturbosch.detekt")
 }
 
 android {
     namespace = "com.mumbicodes.common"
-    compileSdk = 33
 
     defaultConfig {
-        minSdk = 24
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -25,28 +21,15 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
 
     implementation(project(":core:network"))
 
-    implementation(libs.androidx.core.ktx)
-
     // Enables formatting rule set in detekt
     detektPlugins(libs.detekt.formatting)
 
     // ApolloGraphql
     implementation(libs.apollographql)
-
-    testImplementation(libs.junit4)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso)
 }

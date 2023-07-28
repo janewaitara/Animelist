79c8374595b2584333effa788e15edcc21486c41
Waitara <janewaitara99@gmail.com>
2023-07-28 23:56:50 +0300

    Updated the build.gradle in feature:yourList module to use the androidLibraryComposePlugin

--- feature/yourList/build.gradle.kts
+++ feature/yourList/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("animelist.android.library")
+    id("animelist.android.library.compose")
     id("org.jlleitschuh.gradle.ktlint")
     id("io.gitlab.arturbosch.detekt")
 }
@@ -21,22 +22,10 @@ android {
             )
         }
     }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.2"
-    }
 }
 
 dependencies {
-    val composeBom = platform(libs.compose.bom)
-
     implementation(libs.androidx.appCompat)
-
-    implementation(libs.bundles.compose)
-    implementation(libs.compose.navigation)
-    implementation(composeBom)
 
     // Enables formatting rule set in detekt
     detektPlugins(libs.detekt.formatting)

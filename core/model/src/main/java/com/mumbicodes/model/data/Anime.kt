afe906820cf2bc78921991239ec643282eb9b166
Waitara <janewaitara99@gmail.com>
2023-07-31 14:27:30 +0300

    Added a function to map the network searchAnimeQuery Medium to the Anime data class

--- core/model/src/main/java/com/mumbicodes/model/data/Anime.kt
+++ core/model/src/main/java/com/mumbicodes/model/data/Anime.kt
@@ -1,19 +1,21 @@
 package com.mumbicodes.model.data
 
 import com.mumbicodes.network.CharacterQuery
+import com.mumbicodes.network.SearchAnimeQuery
+import com.mumbicodes.network.type.MediaFormat
 
 data class Anime(
     val id: Int,
-    val bannerImage: String?,
+    val bannerImage: String? = null,
     val coverImage: String?,
     val title: AnimeTitle?,
-    val description: String?,
+    val description: String? = "",
     val duration: Int? = 0,
     val genres: List<String?>? = emptyList(),
     val type: MediaType? = null,
     val trailer: MediaTrailer? = null,
     val episodes: Int? = 0,
-    val format: MediaFormat? = null,
+    val format: LocalMediaFormat? = null,
     val characters: List<Character?>? = emptyList(),
     val recommendations: List<Anime?>? = emptyList(),
     val studios: List<String?>? = emptyList()
@@ -35,7 +37,7 @@ data class MediaTrailer(
     val thumbnail: String?
 )
 
-enum class MediaFormat {
+enum class LocalMediaFormat {
     TV,
     TV_SHORT,
     MOVIE,
@@ -45,10 +47,22 @@ enum class MediaFormat {
     MUSIC,
     MANGA,
     NOVEL,
-    ONE_SHOT
+    ONE_SHOT,
+    UNKNOWN
 }
 
-// Mappers
+/**
+ * Mappers
+ * */
+fun SearchAnimeQuery.Medium.toModelAnime() = Anime(
+    id = this.id,
+    coverImage = this.coverImage?.medium,
+    title = this.title?.toAnimeTitle(),
+    episodes = this.episodes,
+    duration = this.duration,
+    format = this.format?.toLocalMediaFormat()
+)
+
 internal fun CharacterQuery.Node.toAnime() = Anime(
     id = this.id,
     coverImage = this.coverImage?.medium,
@@ -62,3 +76,23 @@ private fun CharacterQuery.Title.toAnimeTitle() = AnimeTitle(
     english = this.english,
     romaji = null
 )
+
+private fun SearchAnimeQuery.Title.toAnimeTitle() = AnimeTitle(
+    native = this.native,
+    english = this.english,
+    romaji = null
+)
+
+private fun MediaFormat.toLocalMediaFormat(): LocalMediaFormat = when (this) {
+    MediaFormat.TV -> LocalMediaFormat.TV
+    MediaFormat.TV_SHORT -> LocalMediaFormat.TV_SHORT
+    MediaFormat.MOVIE -> LocalMediaFormat.MOVIE
+    MediaFormat.SPECIAL -> LocalMediaFormat.SPECIAL
+    MediaFormat.OVA -> LocalMediaFormat.OVA
+    MediaFormat.ONA -> LocalMediaFormat.ONA
+    MediaFormat.MUSIC -> LocalMediaFormat.MUSIC
+    MediaFormat.MANGA -> LocalMediaFormat.MANGA
+    MediaFormat.NOVEL -> LocalMediaFormat.NOVEL
+    MediaFormat.ONE_SHOT -> LocalMediaFormat.ONE_SHOT
+    MediaFormat.UNKNOWN__ -> LocalMediaFormat.UNKNOWN
+}

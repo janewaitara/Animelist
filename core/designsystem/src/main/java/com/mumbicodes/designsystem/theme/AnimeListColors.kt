b4af660b9dca0bd3cca60f25188d97c3bf1bac13
Waitara <janewaitara99@gmail.com>
2023-08-10 23:43:29 +0300

    Added color vars initialization

--- core/designsystem/src/main/java/com/mumbicodes/designsystem/theme/AnimeListColors.kt
+++ core/designsystem/src/main/java/com/mumbicodes/designsystem/theme/AnimeListColors.kt
@@ -1,6 +1,10 @@
 package com.mumbicodes.designsystem.theme
 
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.structuralEqualityPolicy
 import androidx.compose.ui.graphics.Color
 
 @Stable
@@ -27,4 +31,54 @@ class AnimeListColors(
     onInfo: Color,
     darkInfoText: Color,
     surfaceBorderInfo: Color
-)
+) {
+    var background by mutableStateOf(background, structuralEqualityPolicy())
+        internal set
+    var cardColors by mutableStateOf(cardColors, structuralEqualityPolicy())
+        internal set
+
+    var primary by mutableStateOf(primary, structuralEqualityPolicy())
+        internal set
+    var onPrimary by mutableStateOf(onPrimary, structuralEqualityPolicy())
+        internal set
+    var surfacePrimaryHover by mutableStateOf(surfacePrimaryHover, structuralEqualityPolicy())
+        internal set
+    var surfacePrimaryDisabled by mutableStateOf(surfacePrimaryDisabled, structuralEqualityPolicy())
+        internal set
+    var surfacePrimaryBorder by mutableStateOf(surfacePrimaryBorder, structuralEqualityPolicy())
+        internal set
+
+    var textStrong by mutableStateOf(textStrong, structuralEqualityPolicy())
+        internal set
+    var textNeutral by mutableStateOf(textNeutral, structuralEqualityPolicy())
+        internal set
+    var textWeak by mutableStateOf(textWeak, structuralEqualityPolicy())
+        internal set
+
+    var error by mutableStateOf(error, structuralEqualityPolicy())
+        internal set
+    var onError by mutableStateOf(onError, structuralEqualityPolicy())
+        internal set
+    var darkErrorText by mutableStateOf(darkErrorText, structuralEqualityPolicy())
+        internal set
+    var surfaceBorderError by mutableStateOf(surfaceBorderError, structuralEqualityPolicy())
+        internal set
+
+    var success by mutableStateOf(success, structuralEqualityPolicy())
+        internal set
+    var onSuccess by mutableStateOf(onSuccess, structuralEqualityPolicy())
+        internal set
+    var darkSuccessText by mutableStateOf(darkSuccessText, structuralEqualityPolicy())
+        internal set
+    var surfaceBorderSuccess by mutableStateOf(surfaceBorderSuccess, structuralEqualityPolicy())
+        internal set
+
+    var info by mutableStateOf(info, structuralEqualityPolicy())
+        internal set
+    var onInfo by mutableStateOf(onInfo, structuralEqualityPolicy())
+        internal set
+    var darkInfoText by mutableStateOf(darkInfoText, structuralEqualityPolicy())
+        internal set
+    var surfaceBorderInfo by mutableStateOf(surfaceBorderInfo, structuralEqualityPolicy())
+        internal set
+}

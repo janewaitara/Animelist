1b11a13080b364fa00f7a97518c5874517b6588d
Waitara <janewaitara99@gmail.com>
2023-08-11 00:54:57 +0300

    Added the colors used in the app

--- core/designsystem/src/main/java/com/mumbicodes/designsystem/theme/AnimeListColors.kt
+++ core/designsystem/src/main/java/com/mumbicodes/designsystem/theme/AnimeListColors.kt
@@ -28,3 +28,56 @@ data class AnimeListColors(
     val darkInfoText: Color,
     val surfaceBorderInfo: Color
 )
+
+// Pumpkin Orange
+val Pumpkin_700 = Color(0xFF492100)
+val Pumpkin_600 = Color(0xFF924200)
+val Pumpkin_500 = Color(0xFFC85A00)
+val Pumpkin_400 = Color(0xFFFF7300)
+val Pumpkin_300 = Color(0xFFFF9B49)
+val Pumpkin_200 = Color(0xFFFFCDA4)
+val Pumpkin_100 = Color(0xFFFFF5ED)
+
+// State colors: DarkPastel Green
+val DarkPastel_700 = Color(0xFF014415)
+val DarkPastel_600 = Color(0xFF026D22)
+val DarkPastel_500 = Color(0xFF02962F)
+val DarkPastel_400 = Color(0xFF03C03C)
+val DarkPastel_300 = Color(0xFF4DFC82)
+val DarkPastel_200 = Color(0xFFA6FEC0)
+val DarkPastel_100 = Color(0xFFE9FFEF)
+
+// State colors: Azure Blue
+val Azure_700 = Color(0xFF002E5B)
+val Azure_600 = Color(0xFF004992)
+val Azure_500 = Color(0xFF006DDB)
+val Azure_400 = Color(0xFF007FFF)
+val Azure_300 = Color(0xFF49A4FF)
+val Azure_200 = Color(0xFFA4D1FF)
+val Azure_100 = Color(0xFFEDF6FF)
+
+// State colors: FireEngine Red
+val FireEngine_700 = Color(0xFF5B0B0F)
+val FireEngine_600 = Color(0xFF891117)
+val FireEngine_500 = Color(0xFFB7171F)
+val FireEngine_400 = Color(0xFFD71B24)
+val FireEngine_300 = Color(0xFFEA535B)
+val FireEngine_200 = Color(0xFFF2989C)
+val FireEngine_100 = Color(0xFFFBDDDE)
+
+// Grey scale
+val Grey_800 = Color(0xFF060315)
+val Grey_700 = Color(0xFF302D3C)
+val Grey_600 = Color(0xFF555166)
+val Grey_500 = Color(0xFF7C798E)
+val Grey_400 = Color(0xFFBFBCCD)
+val Grey_300 = Color(0xFFD7D7E5)
+val Grey_200 = Color(0xFFEDEBF3)
+val Grey_100 = Color(0xFFF4F2FC)
+
+// Backgrounds
+
+val Background_dark = Color(0xFF111111)
+val Background_light = Color(0xFFFBFBFD)
+val Background_white = Color(0xFFFFFFFF)
+val Background_dark_grey = Color(0xFF222129)

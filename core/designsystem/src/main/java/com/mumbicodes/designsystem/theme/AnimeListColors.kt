94a6084872b724bb8de498cf7e25845cc9bc2ec0
Waitara <janewaitara99@gmail.com>
2023-08-10 23:32:24 +0300

    added a colors stable class

---
+++ core/designsystem/src/main/java/com/mumbicodes/designsystem/theme/AnimeListColors.kt
@@ -0,0 +1,30 @@
+package com.mumbicodes.designsystem.theme
+
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.graphics.Color
+
+@Stable
+class AnimeListColors(
+    background: Color,
+    cardColors: Color,
+    primary: Color,
+    onPrimary: Color,
+    surfacePrimaryHover: Color,
+    surfacePrimaryDisabled: Color,
+    surfacePrimaryBorder: Color,
+    textStrong: Color,
+    textNeutral: Color,
+    textWeak: Color,
+    error: Color,
+    onError: Color,
+    darkErrorText: Color,
+    surfaceBorderError: Color,
+    success: Color,
+    onSuccess: Color,
+    darkSuccessText: Color,
+    surfaceBorderSuccess: Color,
+    info: Color,
+    onInfo: Color,
+    darkInfoText: Color,
+    surfaceBorderInfo: Color
+)

4e88000f37f5b5e071dde4a754265ea54d57f93c
Waitara <janewaitara99@gmail.com>
2023-08-15 22:57:23 +0300

    Added AnimeList shapes

---
+++ core/designsystem/src/main/java/com/mumbicodes/designsystem/theme/AnimeListShapes.kt
@@ -0,0 +1,11 @@
+package com.mumbicodes.designsystem.theme
+
+import androidx.compose.foundation.shape.CornerBasedShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+data class AnimeListShapes(
+    val smallShape: CornerBasedShape = RoundedCornerShape(4.dp),
+    val mediumShape: CornerBasedShape = RoundedCornerShape(8.dp),
+    val largeShape: CornerBasedShape = RoundedCornerShape(36.dp)
+)

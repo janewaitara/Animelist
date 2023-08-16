2b37cac9254f33bdea77d156617fcf86b2d9741f
Waitara <janewaitara99@gmail.com>
2023-08-16 11:36:08 +0300

    Added AnimeListTheme function that will be used throught the app to encapsulate the app content

--- core/designsystem/src/main/java/com/mumbicodes/designsystem/theme/AnimeTheme.kt
+++ core/designsystem/src/main/java/com/mumbicodes/designsystem/theme/AnimeTheme.kt
@@ -1,7 +1,13 @@
 package com.mumbicodes.designsystem.theme
 
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 /**
  * Contains functions to access the current theme values
@@ -41,4 +47,28 @@ fun AnimeTheme(
     ) {
         content()
     }
+}
+
+@Composable
+fun AnimeListTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        darkTheme -> darkAnimeListColors()
+        else -> lightAnimeListColors()
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    AnimeTheme(
+        colors = colorScheme,
+        content = content
+    )
 }

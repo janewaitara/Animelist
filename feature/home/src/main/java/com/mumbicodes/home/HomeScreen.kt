00b000ca91860b0a10b8df35ae7ba7f9b029650d
Waitara <janewaitara99@gmail.com>
2023-08-26 15:02:57 +0300

    Added a trending section in the homeScreen

--- feature/home/src/main/java/com/mumbicodes/home/HomeScreen.kt
+++ feature/home/src/main/java/com/mumbicodes/home/HomeScreen.kt
@@ -1,10 +1,27 @@
 package com.mumbicodes.home
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.mumbicodes.designsystem.atoms.Image
+import com.mumbicodes.designsystem.molecules.AnimeSection
+import com.mumbicodes.designsystem.theme.AnimeTheme
+import com.mumbicodes.home.components.VerticalAnimeComponent
 
 @Composable
 fun HomeScreenRoute(
@@ -35,4 +52,63 @@ fun HomeScreen(
     trendingAnimeUiStates: TrendingAnimeStates,
     onAnimeClicked: () -> Unit,
     onSeeAllButtonClicked: () -> Unit
-) {}
+) {
+    Column(
+        modifier = modifier.verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(AnimeTheme.space.space32dp)
+    ) {
+        // TODO which image is this coming from:
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(214.dp),
+            coverImageUrl = null
+        )
+
+        // Trending section
+        when (trendingAnimeUiStates) {
+            is TrendingAnimeStates.TrendingAnimes -> {
+                AnimeSection(
+                    modifier = Modifier.padding(horizontal = AnimeTheme.space.space20dp),
+                    sectionTitle = R.string.popular,
+                    buttonText = R.string.seeAll,
+                    buttonOnClick = onSeeAllButtonClicked
+                ) {
+                    LazyRow(
+                        horizontalArrangement = Arrangement.spacedBy(AnimeTheme.space.space12dp),
+                        contentPadding = PaddingValues(horizontal = AnimeTheme.space.space20dp)
+                    ) {
+                        items(trendingAnimeUiStates.trending) { anime ->
+                            VerticalAnimeComponent(
+                                onClick = onAnimeClicked,
+                                coverImageUrl = anime.coverImage,
+                                animeTitle = anime.title?.english ?: anime.title?.romaji
+                                    ?: anime.title?.native ?: ""
+                            )
+                        }
+                    }
+                }
+            }
+
+            is TrendingAnimeStates.Error -> {
+                // TODO Create an error banner
+                Text(
+                    text = trendingAnimeUiStates.errorMessage,
+                    modifier = Modifier.clickable {
+                        onAnimeClicked()
+                    }
+                )
+            }
+
+            TrendingAnimeStates.Loading -> {
+                // TODO Create a loading skeleton
+                Text(
+                    text = "This is the loading state",
+                    modifier = Modifier.clickable {
+                        onAnimeClicked()
+                    }
+                )
+            }
+        }
+    }
+}

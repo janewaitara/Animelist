6ef3c8d4616d3d05dd311fc9228e4d82e0e82e8a
Waitara <janewaitara99@gmail.com>
2023-09-07 23:34:34 +0300

    atoms: Added a shimmer effect extension function on modifier

--- core/designsystem/src/main/java/com/mumbicodes/designsystem/theme/AnimeListColors.kt
+++ core/designsystem/src/main/java/com/mumbicodes/designsystem/theme/AnimeListColors.kt
@@ -33,7 +33,8 @@ data class AnimeListColors(
     val darkInfoText: Color,
     val surfaceBorderInfo: Color,
     val bottomNavBackground: Color,
-    val bottomNavContentColor: Color
+    val bottomNavContentColor: Color,
+    val shimmerEffectColor: Color
 )
 
 fun lightAnimeListColors(
@@ -60,7 +61,8 @@ fun lightAnimeListColors(
     darkInfoText: Color = Azure_500,
     surfaceBorderInfo: Color = Azure_200,
     bottomNavBackground: Color = Background_white,
-    bottomNavContentColor: Color = Grey_600
+    bottomNavContentColor: Color = Grey_600,
+    shimmerEffectColor: Color = Grey_300
 ) = AnimeListColors(
     background = background,
     cardColors = cardColors,
@@ -85,8 +87,8 @@ fun lightAnimeListColors(
     darkInfoText = darkInfoText,
     surfaceBorderInfo = surfaceBorderInfo,
     bottomNavBackground = bottomNavBackground,
-    bottomNavContentColor = bottomNavContentColor
-
+    bottomNavContentColor = bottomNavContentColor,
+    shimmerEffectColor = shimmerEffectColor
 )
 
 fun darkAnimeListColors(
@@ -113,7 +115,8 @@ fun darkAnimeListColors(
     darkInfoText: Color = Azure_200,
     surfaceBorderInfo: Color = Azure_300,
     bottomNavBackground: Color = Background_dark.copy(alpha = 0.95f),
-    bottomNavContentColor: Color = Grey_400
+    bottomNavContentColor: Color = Grey_400,
+    shimmerEffectColor: Color = Grey_700
 ) = AnimeListColors(
     background = background,
     cardColors = cardColors,
@@ -138,7 +141,8 @@ fun darkAnimeListColors(
     darkInfoText = darkInfoText,
     surfaceBorderInfo = surfaceBorderInfo,
     bottomNavBackground = bottomNavBackground,
-    bottomNavContentColor = bottomNavContentColor
+    bottomNavContentColor = bottomNavContentColor,
+    shimmerEffectColor = shimmerEffectColor
 )
 
 val LocalAnimeListColors = staticCompositionLocalOf<AnimeListColors> {
